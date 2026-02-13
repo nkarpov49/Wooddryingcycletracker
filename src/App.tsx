830da35c1076ebb817f175d27bd008a5d0d30693
nkarpov49 <nkarpov49@gmail.com>
2026-02-13 14:27:11 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link, useLocation } from "react-router-dom";
 import { Home, ArrowLeft, LogOut } from "lucide-react";
-import { Toaster } from "sonner@2.0.3";
+import { Toaster } from "sonner";
 import CycleList from "./components/CycleList";
 import CycleForm from "./components/CycleForm";
 import CycleDetail from "./components/CycleDetail";
